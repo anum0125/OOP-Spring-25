aa93e43a72e6471716bf1b3e29a0d1afbc7e668c
anum0125 <anumsohail2522@gmail.com>
2025-02-10 11:07:25 +0500

    Create Task1.cpp

---
+++ Lab 04/Task1.cpp
@@ -0,0 +1,78 @@
+# include <iostream>
+# include <string>
+# include <cstring>
+
+using namespace std;
+
+class Book
+{
+    string title;
+    double price;
+    int *stock = new int;
+
+    public:
+
+    Book()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            title = "Unknown";
+            price = 0.0;
+            *stock = 30;
+        }
+        
+    }
+
+
+    void lowStockWarning()
+    {
+        if (*stock < 5)
+        {
+            cout << "Low stock warning for " << title << endl;
+        }
+    }
+
+    void updateBookDetails()
+    {
+        
+        cout << "Enter the updated title of the book: " << endl;
+        cin.ignore();
+        getline(cin, title);
+        cout << "Enter the updated price of the book: " << endl;
+        cin >> price;
+        cout << "Enter the updated stock of the book: " << endl;
+        cin >> *stock;
+        lowStockWarning();
+        cout << "Book Details Updated." << endl;
+    }
+    
+
+    void Purchase()
+    {
+        cout << "Enter the quantity of the book you want to buy: " << endl;
+        int q;
+        cin >> q;
+        if (*stock >= q)
+        {
+            *stock -= q;
+            cout << "You have purchased the book: " << endl;
+            lowStockWarning();
+        }
+    }
+         
+
+};
+
+int main()
+{
+    Book b1;
+    Book b2;
+
+    b1.updateBookDetails();
+    b1.Purchase();
+
+    b2.updateBookDetails();
+    b2.Purchase();
+
+
+}
