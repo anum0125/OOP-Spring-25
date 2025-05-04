4b5967b4ffd7cc766a140871ab7a136635cf97e3
anum0125 <anumsohail2522@gmail.com>
2025-05-04 20:41:55 +0500

    Create Task2.cpp

---
+++ Lab 10/Task2.cpp
@@ -0,0 +1,81 @@
+# include <iostream>
+# include <string>
+# include <sstream>
+# include <fstream>
+using namespace std;
+
+class readFile
+{
+    private:
+        ifstream file;
+        string filename;
+
+    public:
+        readFile(string n): filename(n)
+        {
+            file.open(filename);
+        }
+
+        bool isOpen()
+        {
+            return file.is_open();
+        }
+
+        void parsing(string myLine)
+        {
+            istringstream line(myLine);
+            string Type, ID, Name, Year, ExtraData, Certification;
+
+            getline(line, Type, ',');
+            getline(line, ID, ',');
+            getline(line, Name, ',');
+            getline(line, Year, ',');
+            getline(line, ExtraData, ',');
+            getline(line, Certification, ',');
+
+            cout << "Type: " << Type << endl;
+            cout << "ID: " << ID << endl;
+            cout << "Name: " << Name << endl;
+            cout << "Year: " << Year << endl;
+            cout << "ExtraData: " << ExtraData << endl;
+            cout << "Certification: " << Certification << "\n" << endl;
+                
+        }
+
+        void readingFile()
+        {
+            int i = 1;
+            string line;
+            while(getline(file, line))
+            {
+                cout << "Vehicle " << i << ": " << endl;
+                parsing(line);
+                i++;
+            }
+        }
+
+        ~readFile()
+        {
+            if (file.is_open())
+            {
+                file.close();
+            }
+        }
+};
+
+int main()
+{
+    readFile vehicleFile("vehicles.txt");
+
+    if (vehicleFile.isOpen())
+    {
+        vehicleFile.readingFile();
+    }
+    else
+    {
+        cout << "File could not be opened." << endl;
+    }
+
+    return 0;
+    
+}
